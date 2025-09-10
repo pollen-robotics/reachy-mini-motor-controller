a3bd437e9ed5ac6d7e977ff6125f66eb1251c013
CarolinePascal <caroline8.pascal@gmail.com>
2025-09-10 19:45:45 +0200

    fix(super capas startup): fixing voltage error on startup due to the slow charge of super capas. The servos are rebooted when the input voltage reaches 5V.

--- src/control_loop.rs
+++ src/control_loop.rs
@@ -181,6 +181,16 @@ impl ReachyMiniControlLoop {
             }
         }
 
+        // Wait until voltage is stable at 5V
+        let mut current_voltage = read_volt_with_retries(&mut c, read_allowed_retries)?;
+        while current_voltage.iter().any(|&v| v < 50) {
+            std::thread::sleep(Duration::from_millis(100));
+            current_voltage = read_volt_with_retries(&mut c, read_allowed_retries)?;
+        }
+
+        // Reboot all motors on error status
+        c.reboot(true)?;
+
         // Init last position by trying to read current positions
         // If the init fails, it probably means we have an hardware issue
         // so it's better to fail.
