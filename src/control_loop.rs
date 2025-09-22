91df4eccfbef2b372c3c1344af0934ee39ca34a1
CarolinePascal <caroline8.pascal@gmail.com>
2025-09-22 17:02:22 +0200

    chore(formatting): formatting code

--- src/control_loop.rs
+++ src/control_loop.rs
@@ -519,10 +519,7 @@ fn read_volt_with_retries(
             }
         }
     }
-    Err(format!(
-        "Failed to read voltages after {} retries",
-        retries
-    ))
+    Err(format!("Failed to read voltages after {} retries", retries))
 }
 
 fn read_torque_with_retries(
